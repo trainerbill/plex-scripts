42c162a7fea7f80f1b74d6b25339f999127ec374
athroener <athroener@sloth>
2017-10-03 12:41:17 -0500

    Update

--- convert.js
+++ convert.js
@@ -3,7 +3,7 @@ const glob = require("glob");
 const child_process = require("child_process");
 const options = {
 	glob: "/srv/mythtv/plex/**/*.ts",
-	max: 1,
+	max: 1000,
 }
 
 
@@ -17,13 +17,39 @@ function encode(file) {
 	const npath = path.parse(file);
 	delete npath.base;
 	npath.ext = ".mp4";
-	const result = child_process.spawnSync(`ffmpeg -y -i ${file} -c:v libx264 -preset ultrafast -crf 25 -ac 2 -c:a aac -b:a 128k -movflags +faststart -strict -2 ${path.format(npath)}`);
-	
+        const command = `ffmpeg -y -i "${file}" -c:v libx264 -preset ultrafast -crf 25 -ac 2 -c:a aac -b:a 128k -movflags +faststart -strict -2 "${path.format(npath)}"`; 
+	const result = child_process.spawnSync("ffmpeg", [
+		"-y",
+		"-i",
+		file,
+		"-c:v",
+		"libx264",
+		"-preset",
+		"ultrafast",
+		"-crf",
+		"25",
+		"-ac",
+		"2",
+		"-c:a",
+		"aac",
+		"-b:a",
+		"128k",
+		"-movflags",
+		"+faststart",
+		"-strict",
+		"-2",
+		path.format(npath),
+	],
+	{
+		stdio: "inherit",
+	}
+	);
+	console.log("Error", result.error);	
 	console.log(result.output);
 
 	if (result.status === 0) {
 		console.log("Removing file");
-		// child_process.spawnSync(`rm ${file}`);
+		child_process.spawnSync("rm", [file]);
 	}
 }
 
